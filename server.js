d5dedba963aecdeb5d66879ea75d4709d09668ee
gmocore <gmocore@gmail.com>
2020-01-21 11:14:39 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+// import packages
+const express = require('express');
+
+// import routes
+const routes = require('./routes')
+
+// initialize express
+const app = express();
+
+// set port
+const PORT = process.env.PORT || 3001;
+
+// express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets to heroku
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+ 
+//   
+app.use(routes);
+
+
+// start server
+app.listen(PORT, () => console.log(`server listening on ${PORT}`));
