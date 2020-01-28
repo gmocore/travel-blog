5148605e30e7bfd66126e417e9919dac9d39e7e5
gmocore <gmocore@gmail.com>
2020-01-28 12:35:45 -0700

    added social media icons to about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -7,9 +7,28 @@ function About() {
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlcr1rQWN65fZg799YDd_yOJ2Hq_-J9kPzrIQZYyZnuVaDlJo5&s" className="card-img-top" alt="..." />
                 <div className="card-body">
                     <p className="card-text">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit exercitationem, quo cupiditate at tenetur laborum quas sequi minima nostrum officiis sint adipisci laboriosam ullam dolores illum numquam accusantium eum vel eaque recusandae, unde mollitia. Pariatur sunt vero, ipsum rem deleniti natus id nihil libero odio molestiae nam totam minus at.
-                                                                
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Sit exercitationem, quo cupiditate at tenetur laborum quas
+                        sequi minima nostrum officiis sint adipisci laboriosam ullam
+                        dolores illum numquam accusantium eum vel eaque recusandae,
+                        unde mollitia. Pariatur sunt vero, ipsum rem deleniti natus
+                        id nihil libero odio molestiae nam totam minus at.
                     </p>
+
+                </div>
+                <div className="social-icon-container d-flex justify-content-around">
+                    <a href="#">
+                        <i className="fab fa-instagram text-light fa-2x" />
+                    </a>
+                    <a href="#">
+                        <i className="fab fa-facebook-square text-light fa-2x" />
+                    </a>
+                    <a href="#">
+                        <i className="fab fa-twitter-square text-light fa-2x" />
+                    </a>
+                    <a href="#">
+                        <i className="fab fa-snapchat-square text-light fa-2x" />
+                    </a>
                 </div>
             </div>
         </div>
