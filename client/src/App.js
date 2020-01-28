6077fa0df4c3b96c01e052805e68a54dde1382b5
gmocore <gmocore@gmail.com>
2020-01-28 10:41:11 -0700

    added initial content to home page. created nav

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Home from './pages/Home';
 import About from './pages/About';
 import Blog from './pages/Blog';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <Router>
+      <Nav />
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/about" component={About}/>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Home() {
     return (
-        <div>
-            Welcome Home
+        <div >
+            <img src="https://images.pexels.com/photos/772803/pexels-photo-772803.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="img-fluid" alt="Responsive image" />
         </div>
     )
 }
