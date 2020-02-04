eb167fd2bd9d37383713993278ac543ba7080612
gmocore <gmocore@gmail.com>
2020-02-04 13:45:35 -0700

    private route component

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,19 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Blog from './pages/Blog';
 import Nav from './components/Nav';
+import Profile from './components/Profile';
+import PrivateRoute from './components/PrivateRoute';
+import history from './utils/history'
 
 function App() {
   return (
-    <Router>
+    <Router history={history}>
       <Nav />
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/about" component={About}/>
         <Route exact path="/blog" component={Blog}/>
+        <PrivateRoute exact path="/profile" component={Profile} />
       </Switch>
     </Router>
    
