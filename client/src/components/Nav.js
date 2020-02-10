80dd384c073fe845b03b3a26b18f85da71ebae58
gmocore <gmocore@gmail.com>
2020-02-10 12:56:33 -0700

    started active class function

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,8 +3,19 @@ import { Link } from 'react-router-dom';
 import '../App.css';
 import { useAuth0 } from '../react-auth0-spa';
 
+
 function Nav() {
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+    // const activeClass = (e) => {
+    //     console.log(document.getElementsByTagName('a'))
+    //     const link = e.target;
+    //     if (link === e.currentTarget) {
+    //         console.log(true)
+    //         link.classList.add('active')
+    //     } 
+        
+    // }
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark border-bottom shadow">
@@ -15,13 +26,13 @@ function Nav() {
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+                        <Link className="nav-link" to="/" onClick={activeClass}>Home <span className="sr-only">(current)</span></Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/about">About</Link>
+                        <Link className="nav-link" to="/about" onClick={activeClass}>About</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/blog">Blog</Link>
+                        <Link className="nav-link" to="/blog" onClick={activeClass}>Blog</Link>
                     </li>
                 </ul>
             </div>
