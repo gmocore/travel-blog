da25bbec37bd454c1ceee0fb5c8ad56ea0ebbc79
gerritt <gmocore@gmail.com>
2020-02-12 20:57:58 -0700

    added navLink

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,30 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../App.css';
 import { useAuth0 } from '../react-auth0-spa';
 
 
 function Nav() {
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
-    const [ active, setActive ] = React.useState(false)
-    const toggle = () => setActive(!active)
+    // const [ active, setActive ] = React.useState(false)
+    // const toggle = () => setActive(!active)
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark border-bottom shadow">
-            <Link className="navbar-brand" to="#">kB</Link>
+            <NavLink className="navbar-brand" to="#">kB</NavLink>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <Link className={`nav-link ${active ? "active" : ""}`} to="/" onClick={toggle}>Home <span className="sr-only">(current)</span></Link>
+                        <NavLink className={`nav-link`}
+                        activeClassName="active"
+                        exact to="/" >Home </NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className={`nav-link ${active ? "active" : ""}`} to="/about" onClick={toggle}>About</Link>
+                        <NavLink className={`nav-link`}
+                        activeClassName="active"
+                        to="/about" >About</NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className={`nav-link ${active ? "active" : ""}`} to="/blog" onClick={toggle}>Blog</Link>
+                        <NavLink className={`nav-link`}
+                        activeClassName="active"
+                        to="/blog" >Blog</NavLink>
                     </li>
                 </ul>
             </div>
